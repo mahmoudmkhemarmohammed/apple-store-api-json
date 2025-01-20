18f7cb43a696086c97896115563fd231e1e7d1bf
Mahmoud Mkhemar <mahmoudmkhemar0115@gmail.com>
2025-01-20 14:56:54 +0200

    Upload The Api

---
+++ server.js
@@ -0,0 +1,24 @@
+const jsonServer = require('json-server');
+const auth = require('json-server-auth');
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+// قواعد المصادقة
+const rules = auth.rewriter({
+  // Permissions
+  users: 600, // فقط المالك يمكنه الوصول
+  posts: 644, // المالك يمكنه الكتابة، والجميع يمكنهم القراءة
+});
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use(rules); // تطبيق قواعد المصادقة
+server.use(auth); // تفعيل json-server-auth
+server.use(router); // تفعيل Router
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`JSON Server with Auth is running on port ${port}`);
+});
